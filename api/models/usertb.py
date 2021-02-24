10118e567df23b5427d0b3ed91b0aee7cceef0ad
yash nathwani <yashsnathwani15@gmail.com>
2021-02-24 19:40:46 +0530

    change in user file by yash

--- api/models/usertb.py
+++ api/models/usertb.py
@@ -7,9 +7,9 @@ class UserTable(models.Model):
     name = models.CharField(max_length=20)
     uid = models.CharField(max_length=50)
     phone = models.CharField(max_length=10)
-    coordinates = models.CharField(max_length=50)
     email = models.EmailField()
-    address = models.CharField(max_length=50)
+    addressString = models.CharField(max_length=50)
+    addressCoordinate = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.id} - {self.name}'
